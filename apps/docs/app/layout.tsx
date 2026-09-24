8f91c0d94eb942e371491ee4657c94e457a7d5e4
Cris <89263024+cmi23@users.noreply.github.com>
2026-09-24 15:25:43 +0300

    feat: shadcn

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,10 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Geist } from 'next/font/google';
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: {
@@ -20,7 +23,7 @@ const inter = Inter({
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={cn(inter.className, "font-sans", geist.variable)} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
